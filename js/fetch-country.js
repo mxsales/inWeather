297f1a78d7253df39c73441b60260cf06e23e7cd
Marcelo <mrcsales98@gmail.com>
2023-03-29 10:56:16 -0300

    Troca de API das flags

--- js/fetch-country.js
+++ js/fetch-country.js
@@ -1,5 +1,5 @@
 export default async function fetchCountry(country){
 
-  const flag =  await fetch(`https://countryflagsapi.com/png/${country}`);
+  const flag =  await fetch(`https://flagsapi.com/${country}/flat/64.png`);
   return flag.url;
 }
